442cd232e56f5f1803873b483758cf8af48e523a
Sahil Keshao Lede <98254859+sahillede940@users.noreply.github.com>
2023-08-23 20:47:49 +0530

    backend url added

--- src/Url.js
+++ src/Url.js
@@ -1,4 +1,4 @@
-const BACKEND_URL= "http://localhost:5000"
+const BACKEND_URL= "https://web-a-thon-backend.vercel.app"
 
 console.log(BACKEND_URL)
 export const sign_in = `${BACKEND_URL}/api/auth/login/`;
